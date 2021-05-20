53a1df0f57af95b1f12d3a0ce46eb31390c9ca3e
Aleksandr Perevalov <aleksandr.perevalov@hs-anhalt.de>
2021-05-20 18:33:16 +0200

    fixed makefile

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_requirements():
 
 setuptools.setup(
     name="linguaf",
-    version="0.0.6",
+    version="0.0.7",
     author="Aleksandr Perevalov",
     author_email="perevalovproduction@gmail.com",
     description="Python package for calculating famous measures in computational linguistics",
